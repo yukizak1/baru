a0c216d9d293abcc0ba622a4a718dfd382e7a691
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-08-04 17:08:37 +0700

    Update ssh-vmess.sh

--- installer/ssh-vmess.sh
+++ installer/ssh-vmess.sh
@@ -102,36 +102,6 @@ systemctl restart edu-proxyssl
 
 clear
 
-# Getting Proxy Template Ovpn
-wget -q -O /usr/local/bin/edu-proxyovpn https://raw.githubusercontent.com/4hidessh/baru/main/proxy-templatedovpn.py
-chmod +x /usr/local/bin/edu-proxyovpn
-
-# Installing Service
-cat > /etc/systemd/system/edu-proxyovpn.service << END
-[Unit]
-Description=Python Edu Ovpn Proxy By Radenpancal Service
-Documentation=https://hidessh.com
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxyovpn 2086
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable edu-proxyovpn
-systemctl restart edu-proxyovpn
-
-clear
-
 # nano /etc/bin/wstunnel
 cat > /etc/bin/wstunnel <<-END
 #!/bin/sh -e
