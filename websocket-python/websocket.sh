98623a4856ef859569a56c6e5f991b79a2de9fd1
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-08-09 01:40:32 +0700

    Update websocket.sh

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -6,6 +6,7 @@ cd
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/openssh-socket.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/4hidessh/baru/main/websocket-python/ws-dropbear
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/ws-stunnel
 wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/ws-stunnel && chmod +x /usr/local/bin/ws-ovpn
 
@@ -24,9 +25,16 @@ wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com
 ##System Websocket-OpenVPN Python
 wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/ws-stunnel && chmod +x /etc/systemd/system/ws-ovpn.service
 
+
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/4hidessh/baru/main/websocket-python/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+
+
 #restart service
 #restart system
 systemctl daemon-reload
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
 
 #Enable & Start & Restart ws-openssh service
 systemctl enable ws-openssh.service
