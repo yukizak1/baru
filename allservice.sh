518f804c02de5c5495a15fb9708eeb2615fa093e
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-10-16 14:43:54 +0700

    Update allservice.sh

--- allservice.sh
+++ allservice.sh
@@ -1,8 +1,8 @@
-/etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/stunnel4 restart
 /etc/init.d/openvpn restart
 /etc/init.d/squid restart
+service sslh restart
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
