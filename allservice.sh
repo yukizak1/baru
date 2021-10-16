000e8fdbca0036a4797aac4570d54beb22377510
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-10-16 14:37:49 +0700

    Create allservice.sh

---
+++ allservice.sh
@@ -0,0 +1,13 @@
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/openvpn restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+echo -e ""
+echo -e "======================================"
+echo -e ""
+echo -e "          Service/s Restarted         "
+echo -e "======================================"
